6e32291dc5697d126d4e910344ba9877bce51464
ultygold <120239343+ultygold@users.noreply.github.com>
2022-12-10 11:26:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -25,12 +25,22 @@ browser.set_window_size(1920, 1080)
 # time.sleep(1)
 
 browser.get('https://github.com/')
-time.sleep(5)
+time.sleep(3)
 
-browser.find_element(By.XPATH, '/html/body/div[1]/div[1]/header/div/div[2]/div/div/a').click()
-time.sleep(6)
-time.sleep(1)
-asas = "qlichevasadbek@yandex.ru"
+browser.find_element(By.XPATH, '/html/body/div[1]/div[1]/header/div/div[2]/div/div/div[2]/a').click()
+time.sleep(3)
+gold = "ultygold"
+browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/main/div/div[4]/form/input[2]').send_keys(gold)
+time.sleep(3)
+ulty = "AB@8bzWWD9rtGKi"
+browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/main/div/div[4]/form/div/input[1]').send_keys(ulty)
 
-browser.find_element(By.XPATH, '/html/body/div[1]/div[4]/main/div[2]/text-suggester/div[1]/form/div[1]/div[2]/div/auto-check/input[1]').send_keys(asas)
+time.sleep(3)
+browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/main/div/div[4]/form/div/input[11]').click()
 
+time.sleep(3)
+browser.find_element(By.XPATH, '/html/body/div[1]/div[5]/div/aside/div/loading-context/div/div[1]/div/ul/li/div/div/a').click()
+time.sleep(3)
+
+browser.find_element(By.XPATH, '/html/body/div[1]/div[5]/div/main/turbo-frame/div/div/div/div[3]/div[1]/div[3]/div[3]/div[1]/div[5]/div[2]/span/a').click()
+time.sleep(3)
